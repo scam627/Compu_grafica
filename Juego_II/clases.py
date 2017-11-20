3769f169c1bf8dafeb6321db1dd57931fe221d74
scam627 <cardona-627@utp.edu.co>
2017-11-19 23:16:34 -0500

    Alpha version 1.0 funcional

--- Juego_II/clases.py
+++ Juego_II/clases.py
@@ -68,6 +68,207 @@ class Vidas(pg.sprite.Sprite):
 			self.image=self.m[0][0]
 		else:
 			self.image=self.m[0][1]
+class Enemyss(pg.sprite.Sprite):
+    def __init__(self,d):
+        pg.sprite.Sprite.__init__(self)
+        self.name='Recursos/Personajes/enemy.png'
+       	self.init_constantes()
+       	self.init_bool_variables()
+       	self.init_variables(d)
+
+    def update(self):
+        if self.mover :
+        	self.movimiento()
+    	if self.combo:  
+    		self.animacion()
+    	if self.golpeo:
+    		self.golpe()
+        if self.rect.x>=ANCHO-self.rect[2]:
+            self.var_x=0
+        if self.rect.x<=0:
+            self.rect.x=0
+            self.var_x=0
+        if not self.orien:
+        	if self.x<len(self.m[self.dir+8]):
+    			self.image=self.m[self.dir+8][self.x]
+    	else:
+        	if self.x<len(self.m[self.dir+8]):
+    			self.image=self.m[self.dir][self.x]
+
+    def init_bool_variables(self):
+    	print "Iniciando valores booleanos"
+    	self.mover=False
+    	self.golpeo=False
+    	self.combo=True
+    	self.orien=True
+
+    def init_variables(self,d):
+    	print "Iniciando valores variables"
+    	self.my=d
+    	self.salud=100
+    	self.dir=0
+    	self.x=0
+    	self.get_mat()
+    	self.image=self.m[self.dir][self.x]
+        self.rect=self.image.get_rect()
+        self.rect.y=self.my-self.image.get_size()[1]
+        self.var_x=0
+
+    def init_constantes(self):
+    	print "Iniciando valores constantes"
+    	self.gravedad=5
+    	self.max_y_salto=112
+    	self.sheet=pg.image.load(self.name).convert_alpha()
+
+    def get_mat(self):
+		ancho_img,alto_img=self.sheet.get_size()
+		row_img=[]
+		self.m=[]
+		recorte=self.sheet.subsurface((0,319,34,40))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((36,319,34,40))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((72,319,34,40))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((108,319,34,40))
+		row_img.append(recorte)
+		self.m.append(row_img)
+		row_img=[]
+		recorte=self.sheet.subsurface((0,361,42,41))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((44,361,42,41))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((88,361,42,41))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((132,361,42,41))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((175,361,42,41))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((218,361,42,41))
+		row_img.append(recorte)
+		self.m.append(row_img)
+		row_img=[]
+		recorte=self.sheet.subsurface((0,404,32,48))
+		row_img.append(recorte)
+		self.m.append(row_img)
+		row_img=[]
+		recorte=self.sheet.subsurface((0,454,33,40))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((35,454,33,40))
+		row_img.append(recorte)
+		self.m.append(row_img)
+		row_img=[]
+		recorte=self.sheet.subsurface((0,496,32,32))
+		row_img.append(recorte)
+		self.m.append(row_img)
+		row_img=[]
+		recorte=self.sheet.subsurface((0,530,42,30))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((44,530,42,30))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((88,530,42,30))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((132,530,42,30))
+		row_img.append(recorte)
+		self.m.append(row_img)
+		row_img=[]
+		recorte=self.sheet.subsurface((0,562,32,32))
+		row_img.append(recorte)
+		self.m.append(row_img)
+		row_img=[]
+		recorte=self.sheet.subsurface((0,596,50,40))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((52,596,50,40))
+		row_img.append(recorte)    
+		recorte=self.sheet.subsurface((104,596,50,40))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((156,596,50,40))
+		row_img.append(recorte)
+		self.m.append(row_img)
+		row_img=[]
+		recorte=self.sheet.subsurface((571,319,34,40))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((535,319,34,40))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((499,319,34,40))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((463,319,34,40))
+		row_img.append(recorte)
+		self.m.append(row_img)
+		row_img=[]
+		recorte=self.sheet.subsurface((563,361,42,41))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((519,361,42,41))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((475,361,42,41))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((431,361,42,41))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((387,361,42,41))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((343,361,42,41))
+		row_img.append(recorte)
+		self.m.append(row_img)
+		row_img=[]
+		recorte=self.sheet.subsurface((573,404,32,48))
+		row_img.append(recorte)
+		self.m.append(row_img)
+		row_img=[]
+		recorte=self.sheet.subsurface((572,454,33,40))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((537,454,33,40))
+		row_img.append(recorte)
+		self.m.append(row_img)
+		row_img=[]
+		recorte=self.sheet.subsurface((573,496,32,32))
+		row_img.append(recorte)
+		self.m.append(row_img)
+		row_img=[]
+		recorte=self.sheet.subsurface((563,530,42,30))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((519,530,42,30))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((475,530,42,30))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((431,530,42,30))
+		row_img.append(recorte)
+		self.m.append(row_img)
+		row_img=[]
+		recorte=self.sheet.subsurface((573,562,32,32))
+		row_img.append(recorte)
+		self.m.append(row_img)
+		row_img=[]
+		recorte=self.sheet.subsurface((555,596,50,40))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((503,596,50,40))
+		row_img.append(recorte)    
+		recorte=self.sheet.subsurface((451,596,50,40))
+		row_img.append(recorte)
+		recorte=self.sheet.subsurface((399,596,50,40))
+		row_img.append(recorte)
+		self.m.append(row_img)
+    
+    def golpe(self):
+    	if self.x<len(self.m[self.dir])-1:
+    		self.x+=1
+    	else:
+    		self.x=0
+
+    def movimiento(self):
+    	self.rect.x+=self.var_x
+    	self.rect.y=self.my-self.image.get_size()[1]
+    	if self.x < len(self.m[self.dir])-1:
+            self.x+=1
+        else:
+            self.x=0
+
+    def animacion(self):
+    	if self.x < len(self.m[self.dir])-1:
+            self.x+=1
+        else:
+        	self.dir=0
+        	self.x=0
+
 class Enemys(pg.sprite.Sprite):
     def __init__(self,d):
         pg.sprite.Sprite.__init__(self)
@@ -178,11 +379,11 @@ class Enemys(pg.sprite.Sprite):
 		row_img=[]
 		recorte=self.sheet.subsurface((0,277,50,40))
 		row_img.append(recorte)
-		recorte=self.sheet.subsurface((48,277,50,40))
+		recorte=self.sheet.subsurface((52,277,50,40))
 		row_img.append(recorte)    
-		recorte=self.sheet.subsurface((98,277,50,40))
+		recorte=self.sheet.subsurface((104,277,50,40))
 		row_img.append(recorte)
-		recorte=self.sheet.subsurface((148,277,50,40))
+		recorte=self.sheet.subsurface((156,277,50,40))
 		row_img.append(recorte)
 		self.m.append(row_img)
 		row_img=[]
@@ -224,13 +425,13 @@ class Enemys(pg.sprite.Sprite):
 		row_img.append(recorte)
 		self.m.append(row_img)
 		row_img=[]
-		recorte=self.sheet.subsurface((531,209,42,30))
+		recorte=self.sheet.subsurface((563,530,42,30))
 		row_img.append(recorte)
-		recorte=self.sheet.subsurface((487,209,42,30))
+		recorte=self.sheet.subsurface((519,530,42,30))
 		row_img.append(recorte)
-		recorte=self.sheet.subsurface((443,209,42,30))
+		recorte=self.sheet.subsurface((475,530,42,30))
 		row_img.append(recorte)
-		recorte=self.sheet.subsurface((399,209,42,30))
+		recorte=self.sheet.subsurface((431,530,42,30))
 		row_img.append(recorte)
 		self.m.append(row_img)
 		row_img=[]
@@ -268,9 +469,6 @@ class Enemys(pg.sprite.Sprite):
         else:
         	self.dir=0
         	self.x=0
-            
-
-
 
 class Naruto(pg.sprite.Sprite):
     def __init__(self,d):
--- Juego_II/main.py
+++ Juego_II/main.py
@@ -20,11 +20,38 @@ def dibujar(pantalla,lo):
         t+=1
 
 def final_game(pantalla):
-    muerto=pygame.image.load('Recursos/Objetos/gameover.jpg')
+    muerto=pygame.image.load('Recursos/Objetos/gameover.png')
     pantalla.blit(muerto,[150,80])
     pygame.display.flip()
+    reloj=pygame.time.Clock()
+    reloj.tick(1)
 
-def inicio(enemy,jugador):
+def pausa(pantalla):
+    pause=pygame.image.load('Recursos/Objetos/pausa.png')
+    pausa=pygame.image.load('Recursos/Objetos/pause.png')
+    pantalla.blit(pause,[200,0])
+    pantalla.blit(pausa,[10,100])
+    pygame.display.flip()
+
+def portada(pantalla):
+    por=pygame.image.load('Recursos/Objetos/portada.png')
+    fondo=pygame.image.load('Recursos/Objetos/fondo.jpg')
+    jugar=pygame.image.load('Recursos/Objetos/jugar.png')
+    controles=pygame.image.load('Recursos/Objetos/contr.png')
+    pantalla.blit(fondo,[0,0])
+    pantalla.blit(por,[0,160])
+    pantalla.blit(jugar,[250,180])
+    pantalla.blit(controles,[300,10])
+    pygame.display.flip()
+
+def pasar(pantalla):
+    fondo=pygame.image.load('Recursos/Objetos/fondo.jpg')
+    level=pygame.image.load('Recursos/Objetos/level_2.png')
+    pantalla.blit(fondo,[0,0])
+    pantalla.blit(level,[200,10])
+    pygame.display.flip()
+
+def inicio(enemy,jugador,enemys,general,pantalla,lo,reloj):
     jugador.dir=11
     enemy.dir=2
     enemy.rect.y=40
@@ -70,10 +97,8 @@ def persecution(enemy,jugador):
             enemy.golpeo=True
             enemy.mover=False
             enemy.combo=False
-if __name__== '__main__':
-    pygame.init()
-    pygame.display.set_caption('Naruto ninja')
-    pantalla=pygame.display.set_mode([ANCHO,ALTO])
+def level_one(pantalla):
+    score=0
     pantalla.fill(NEGRO)
     lv=[]
     one=Vidas(20,270)
@@ -111,11 +136,15 @@ if __name__== '__main__':
     general.draw(pantalla)
     pygame.display.flip()
     fin=False
-    inicio(en,bl)
-    number_enemys=5
+    inicio(en,bl,enemys,general,pantalla,lo,reloj)
+    number_enemys=7
+    number_enemys_2=0
     bl.dir=11
     en.dir=0
+    second=False
     actual=11
+    PAUSE=False
+    GAMEOVER=False
 #    pygame.mixer.music.load("sound.mp3")
 #    pygame.mixer.music.play(1)
     while not fin:
@@ -123,6 +152,11 @@ if __name__== '__main__':
             if event.type==pygame.QUIT:
                 sys.exit()
             if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_p:
+                    if PAUSE:
+                        PAUSE=False
+                    else:
+                        PAUSE=True
                 if event.key==pygame.K_d or event.key==pygame.K_RIGHT:
                     bl.var_x=8
                     bl.var_y=0
@@ -201,18 +235,7 @@ if __name__== '__main__':
                                 reloj.tick(TIME)
                     actual=bl.dir
                 if event.key==pygame.K_w:
-                    bl.estatico=True
-                    bl.mover=False
-                    bl.dir=0
-                    bl.salto=False
-                    bl.x=0
-                    for i in range(len(bl.m[bl.dir])):
-                        bl.x=i
-                        dibujar(pantalla,lo)
-                        actualizar(pantalla,lls)
-                        pygame.display.flip()
-                        reloj.tick(TIME)
-                    actual=bl.dir
+                    print "hola"
             if event.type==pygame.KEYUP:
                 bl.var_x=0
                 bl.dir=11
@@ -230,6 +253,9 @@ if __name__== '__main__':
                 bl.salto=False
                 bl.x=0
                 if pygame.mouse.get_pressed() == (1,0,0):
+                    pygame.mixer.music.load("Recursos/Sonidos/glope.mp3")
+                    pygame.mixer.music.set_volume(2)
+                    pygame.mixer.music.play(1)
                     for i in range(len(bl.m[bl.dir])):
                         bl.x=i
                         dibujar(pantalla,lo)
@@ -239,6 +265,9 @@ if __name__== '__main__':
                 if pygame.mouse.get_pressed() == (0,1,0):
                     bl.dir=2
                     bl.x=0
+                    pygame.mixer.music.load("Recursos/Sonidos/glope.mp3")
+                    pygame.mixer.music.set_volume(2)
+                    pygame.mixer.music.play(1)
                     for i in range(len(bl.m[bl.dir])):
                         bl.x=i
                         dibujar(pantalla,lo)
@@ -248,6 +277,303 @@ if __name__== '__main__':
                 if pygame.mouse.get_pressed() == (0,0,1):
                     bl.dir=1
                     bl.x=0
+                    pygame.mixer.music.load("Recursos/Sonidos/glope.mp3")
+                    pygame.mixer.music.set_volume(2)
+                    pygame.mixer.music.play(1)
+                    for i in range(len(bl.m[bl.dir])):
+                        bl.x=i
+                        dibujar(pantalla,lo)
+                        actualizar(pantalla,lls)
+                        reloj.tick(TIME)
+                if pygame.mouse.get_pressed() == (1,0,1):
+                    return [len(lv),bl.salud,score]
+                actual=bl.dir
+            if event.type==pygame.MOUSEBUTTONUP:
+                bl.var_x=0
+                bl.dir=11
+                bl.mover=False
+                bl.estatico=True
+                bl.combo=False
+                bl.salto=False
+                bl.subir=False
+                bl.caer=False
+                actual=bl.dir
+        persecution(en,bl)
+        col_je=pygame.sprite.spritecollide(bl,enemys,False)
+        col_ej=pygame.sprite.spritecollide(en,jugadores,False)
+        for element in col_je:
+            if (bl.dir==0 or bl.dir==1 or bl.dir==2) and (en.rect.x>bl.rect.x-30 and en.rect.x<bl.rect.x+30):
+                en.golpeo=True
+                en.salud-=10
+                score+=1
+            else:
+                en.golpeo=False
+        for element in col_ej:
+            if en.dir==7 and (bl.dir != 0 and bl.dir != 1 and bl.dir != 2) and existe:
+                bl.dir=12
+                bl.salud-=2
+                pygame.mixer.music.load("Recursos/Sonidos/dolor.mp3")
+                pygame.mixer.music.set_volume(2)
+                pygame.mixer.music.play(1)
+            else:
+                bl.dir=actual
+        if bl.salud <= 0:
+            if len(lv) > 0:
+                lv[(len(lv)-1)].existo=False
+                lv.pop()
+                bl.salud=100
+        if en.salud <= 0 and existe:
+            en.dir=4
+            en.x=0
+            en.mover=False
+            en.golpeo=False
+            en.combo=False
+            score+=3
+            dibujar(pantalla,lo)
+            actualizar(pantalla,lls)
+            reloj.tick(TIME)
+            en.dir=5
+            for i in range(len(en.m[en.dir])):
+                en.x=i
+                en.rect.x-=4
+                en.rect.y+=4
+                dibujar(pantalla,lo)
+                actualizar(pantalla,lls)
+                reloj.tick(5)
+                existe=False
+            general.remove(en)
+            col_ej=None
+            col_je=None
+            if number_enemys > 0:
+                en=Enemys(260)
+                general.add(en)
+                existe=True
+                inicio(en,bl,enemys,general,pantalla,lo,reloj)
+                number_enemys-=1
+            else:
+                if not second:
+                    number_enemys_2=7
+                second=True
+            if second:
+                print number_enemys_2
+                if number_enemys_2 > 0:
+                    print "crea"
+                    en=Enemyss(260)
+                    general.add(en)
+                    existe=True
+                    inicio(en,bl,enemys,general,pantalla,lo,reloj)
+                    number_enemys_2-=1
+                else:
+                    existe=False
+                    return [len(lv),bl.salud,score]
+        if not PAUSE:
+            if not GAMEOVER:
+                if len(lv)==0:
+                    dibujar(pantalla,lo)
+                    actualizar(pantalla,lls)
+                    GAMEOVER=True
+                    print "gameover"
+                    reloj.tick(TIME)
+                else:
+                    dibujar(pantalla,lo)
+                    actualizar(pantalla,lls)
+            else:
+                final_game(pantalla)
+                reloj.tick(5)
+                print "Game Over"
+                return [len(lv),bl.salud,score]
+        else:
+            pausa(pantalla)
+        bl.dir=actual
+        reloj.tick(TIME)
+        #bl.rect.x-=2
+def level_two(pantalla,nv,salud,score):
+    pantalla.fill(NEGRO)
+    lv=[]
+    one=Vidas(20,270)
+    two=Vidas(80,270)
+    three=Vidas(140,270)
+    lv.append(one)
+    lv.append(two)
+    lv.append(three)
+    n=3-nv
+    for i in range(n):
+        lv[(len(lv)-1)].existo=False
+        lv.pop()
+    en=Enemys(260)
+    bl=Naruto(265)
+    bl.salud=salud
+    bl.rect.x=300
+    lo=[]
+    prado=Prado()
+    barranco=Barranco()
+    arbol=Arboles()
+    fondo=Fondo(0,0)
+    fondoo=Fondo(512,0)
+    lo.append(fondo)
+    lo.append(fondoo)
+    lo.append(arbol)
+    lo.append(barranco)
+    lo.append(prado)
+    jugadores=pygame.sprite.Group()
+    enemys=pygame.sprite.Group()
+    lls=[]
+    general=pygame.sprite.Group()
+    lls.append(general)
+    general.add(one)
+    general.add(two)
+    general.add(three)
+    general.add(bl)
+    jugadores.add(bl)
+    existe=True
+    second=False
+    reloj=pygame.time.Clock()
+    general.draw(pantalla)
+    pygame.display.flip()
+    fin=False
+    inicio(en,bl,enemys,general,pantalla,lo,reloj)
+    number_enemys=7
+    bl.dir=11
+    en.dir=0
+    actual=11
+    TERM=False
+    PAUSE=False
+    GAMEOVER=False
+#    pygame.mixer.music.load("sound.mp3")
+#    pygame.mixer.music.play(1)
+    while not fin:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                sys.exit()
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_p:
+                    if PAUSE:
+                        PAUSE=False
+                    else:
+                        PAUSE=True
+                if event.key==pygame.K_d or event.key==pygame.K_RIGHT:
+                    bl.var_x=8
+                    bl.var_y=0
+                    bl.dir=6
+                    bl.mover=True
+                    bl.combo=False
+                    bl.estatico=False
+                    bl.orien=True
+                    actual=bl.dir
+                if event.key==pygame.K_a or event.key==pygame.K_LEFT:
+                    bl.var_x=-8
+                    bl.var_y=0
+                    bl.dir=6
+                    bl.mover=True
+                    bl.combo=False
+                    bl.estatico=False
+                    bl.orien=False
+                    actual=bl.dir
+                if event.key==pygame.K_s or event.key==pygame.K_DOWN:
+                    bl.var_x=0
+                    bl.dir=11
+                    bl.mover=False
+                    bl.combo=False
+                    bl.estatico=True
+                    actual=bl.dir
+                if event.key==pygame.K_SPACE:
+                    bl.estatico=True
+                    bl.salto=True
+                    bl.dir=8
+                    if not bl.mover:
+                        for j in range(6):
+                            for i in range(len(bl.m[bl.dir])):    
+                                bl.x=i
+                                bl.rect.y-=bl.gravedad
+                                dibujar(pantalla,lo)
+                                actualizar(pantalla,lls)
+                                pygame.display.flip()
+                                reloj.tick(TIME)
+                        bl.dir=9
+                        for j in range(6):
+                            for i in range(len(bl.m[bl.dir])):    
+                                bl.x=i
+                                bl.rect.y+=bl.gravedad
+                                dibujar(pantalla,lo)
+                                actualizar(pantalla,lls)
+                                pygame.display.flip()
+                                reloj.tick(TIME)
+                    else:
+                        bl.mover=False
+                        for j in range(6):
+                            for i in range(len(bl.m[bl.dir])):    
+                                bl.x=i
+                                if bl.orien:
+                                    bl.rect.x+=VAL_X[i]
+                                    bl.rect.y-=VAL_Y[i]
+                                else:
+                                    bl.rect.x-=VAL_X[i]
+                                    bl.rect.y-=VAL_Y[i]
+                                dibujar(pantalla,lo)
+                                actualizar(pantalla,lls)
+                                pygame.display.flip()
+                                reloj.tick(TIME)
+                        bl.dir=9
+                        for j in range(6):
+                            for i in range(len(bl.m[bl.dir])):    
+                                bl.x=i
+                                if bl.orien:
+                                    bl.rect.x+=VAL_X[i]
+                                    bl.rect.y+=VAL_Y[i]
+                                else:
+                                    bl.rect.x-=VAL_X[i]
+                                    bl.rect.y+=VAL_Y[i]
+                                dibujar(pantalla,lo)
+                                actualizar(pantalla,lls)
+                                pygame.display.flip()
+                                reloj.tick(TIME)
+                    actual=bl.dir
+                if event.key==pygame.K_w:
+                    print "hola"
+            if event.type==pygame.KEYUP:
+                bl.var_x=0
+                bl.dir=11
+                bl.mover=False
+                bl.estatico=True
+                bl.combo=False
+                bl.salto=False
+                bl.subir=False
+                bl.caer=False
+                actual=bl.dir
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                bl.estatico=False
+                bl.mover=False
+                bl.dir=0
+                bl.salto=False
+                bl.x=0
+                if pygame.mouse.get_pressed() == (1,0,0):
+                    pygame.mixer.music.load("Recursos/Sonidos/glope.mp3")
+                    pygame.mixer.music.set_volume(2)
+                    pygame.mixer.music.play(1)
+                    for i in range(len(bl.m[bl.dir])):
+                        bl.x=i
+                        dibujar(pantalla,lo)
+                        actualizar(pantalla,lls)
+                        pygame.display.flip()
+                        reloj.tick(TIME)
+                if pygame.mouse.get_pressed() == (0,1,0):
+                    bl.dir=2
+                    bl.x=0
+                    pygame.mixer.music.load("Recursos/Sonidos/glope.mp3")
+                    pygame.mixer.music.set_volume(2)
+                    pygame.mixer.music.play(1)
+                    for i in range(len(bl.m[bl.dir])):
+                        bl.x=i
+                        dibujar(pantalla,lo)
+                        actualizar(pantalla,lls)
+                        pygame.display.flip()
+                        reloj.tick(TIME)
+                if pygame.mouse.get_pressed() == (0,0,1):
+                    bl.dir=1
+                    bl.x=0
+                    pygame.mixer.music.load("Recursos/Sonidos/glope.mp3")
+                    pygame.mixer.music.set_volume(2)
+                    pygame.mixer.music.play(1)
                     for i in range(len(bl.m[bl.dir])):
                         bl.x=i
                         dibujar(pantalla,lo)
@@ -268,15 +594,19 @@ if __name__== '__main__':
         col_je=pygame.sprite.spritecollide(bl,enemys,False)
         col_ej=pygame.sprite.spritecollide(en,jugadores,False)
         for element in col_je:
-            if bl.dir==0 or bl.dir==1 or bl.dir==2:
+            if (bl.dir==0 or bl.dir==1 or bl.dir==2) and (en.rect.x>bl.rect.x-30 and en.rect.x<bl.rect.x+30):
                 en.golpeo=True
-                en.salud-=10
+                en.salud-=8
+                score+=1
             else:
                 en.golpeo=False
         for element in col_ej:
-            if en.dir==7 and (bl.dir != 0 and bl.dir != 1 and bl.dir != 2) and number_enemys>0:
+            if en.dir==7 and (bl.dir != 0 and bl.dir != 1 and bl.dir != 2) and existe:
                 bl.dir=12
-                bl.salud-=2
+                bl.salud-=5
+                pygame.mixer.music.load("Recursos/Sonidos/dolor.mp3")
+                pygame.mixer.music.set_volume(2)
+                pygame.mixer.music.play(1)
             else:
                 bl.dir=actual
         if bl.salud <= 0:
@@ -293,6 +623,7 @@ if __name__== '__main__':
             dibujar(pantalla,lo)
             actualizar(pantalla,lls)
             reloj.tick(TIME)
+            score+=3
             en.dir=5
             for i in range(len(en.m[en.dir])):
                 en.x=i
@@ -303,27 +634,52 @@ if __name__== '__main__':
                 reloj.tick(5)
                 existe=False
             general.remove(en)
-            if number_enemys>0:
+            col_ej=None
+            col_je=None
+            if number_enemys > 0:
                 en=Enemys(260)
                 general.add(en)
                 existe=True
-                inicio(en,bl)
+                inicio(en,bl,enemys,general,pantalla,lo,reloj)
                 number_enemys-=1
             else:
-                existe=False
-        if not GAMEOVER:
-            if len(lv)==0:
-                dibujar(pantalla,lo)
-                actualizar(pantalla,lls)
-                GAMEOVER=True
-                print "gameover"
-                reloj.tick(TIME)
+                if not second:
+                    number_enemys_2=7
+                second=True
+            if second:
+                print number_enemys_2
+                if number_enemys_2 > 0:
+                    print "crea"
+                    en=Enemyss(260)
+                    general.add(en)
+                    existe=True
+                    inicio(en,bl,enemys,general,pantalla,lo,reloj)
+                    number_enemys_2-=1
+                else:
+                    existe=False
+                    TERM=True
+        if not PAUSE:
+            if not GAMEOVER:
+                if len(lv)==0:
+                    dibujar(pantalla,lo)
+                    actualizar(pantalla,lls)
+                    GAMEOVER=True
+                    print "gameover"
+                    reloj.tick(TIME)
+                else:
+                    dibujar(pantalla,lo)
+                    actualizar(pantalla,lls)
+
             else:
-                dibujar(pantalla,lo)
-                actualizar(pantalla,lls)
+                final_game(pantalla)
+                reloj.tick(5)
+                print "Game Over"
+                break
         else:
-            final_game(pantalla)
-            print "Game Over"
+            pausa(pantalla)
+        if TERM==True:
+            reloj.tick(5)
+            break
         bl.dir=actual
         reloj.tick(TIME)
         #bl.rect.x-=2
